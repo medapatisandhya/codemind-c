c155a3a1e31d12d3365b16bfea6890fb2e23d8bd
sandhya <120093615+medapatisandhya@users.noreply.github.com>
2023-01-05 08:13:04 -0800

    #include <stdio.h> int main() {     int n,m;     scanf("%d",&n);     int r=0,s=0,cnt=0;     while(n)     {         r=(r*10)+(n%10);         n=n/10;     }     while(r)     {         if(r%10==6 && cnt ==0)         {             s=(s*10)+9;             cnt++;         }         else         {             s=(s*10)+(r%10);         }         r=r/10;     }     printf("%d",s);
    }

---
+++ Maximum_69_Number.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+int main()
+{
+    int n,m;
+    scanf("%d",&n);
+    int r=0,s=0,cnt=0;
+    while(n)
+    {
+        r=(r*10)+(n%10);
+        n=n/10;
+    }
+    while(r)
+    {
+        if(r%10==6 && cnt ==0)
+        {
+            s=(s*10)+9;
+            cnt++;
+        }
+        else
+        {
+            s=(s*10)+(r%10);
+        }
+        r=r/10;
+    }
+    printf("%d",s);
+    
+}
