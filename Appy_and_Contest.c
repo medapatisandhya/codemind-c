57eedb490d7b0f6c4d9ccc563f30438871ee3269
sandhya <120093615+medapatisandhya@users.noreply.github.com>
2023-05-12 11:42:47 +0530

    #include <stdio.h> int main() {     int j,m;     scanf("%d",&m);     for(j=1;j<=m;j++)     {     int n,a,b,k,z[4]={n,a,b,k},l,g,c,i;     scanf("%d%d%d%d",&n,&a,&b,&k);     for(i=1;i<=a&&i<=b;i++)     {         if(a%i==0 && b%i==0)         {             g=i;         }     }     l=(a*b)/g;     c=(n/a)+(n/b)-(n*2)/l;     if(c>=k)     {         printf("Win ");     }     else     {         printf("Lose ");     }     } }

--- Appy_and_Contest.c
+++ Appy_and_Contest.c
@@ -1,35 +1,30 @@
-#include<stdio.h>
-#include<math.h>
-int gcd(int a,int b)
-{
-  if(b==0)
-  {
-      return abs(a);
-  }
-  else
-  {
-      return gcd(b,a%b);
-  }
-}
+#include <stdio.h>
 int main()
 {
-    int n,a,b,k,i,r,lcm,gcf,c,x;
-    scanf("%d",&r);
-    for(i=1;i<=r;i++)
+    int j,m;
+    scanf("%d",&m);
+    for(j=1;j<=m;j++)
     {
-        scanf("%d %d %d %d",&n,&a,&b,&k);
-        c=gcd(a,b);
-        lcm=(a*b)/c;
-        x=(n/a)+(n/b)-(2*n/lcm);
-        if(x>=k)
+    int n,a,b,k,z[4]={n,a,b,k},l,g,c,i;
+    scanf("%d%d%d%d",&n,&a,&b,&k);
+    for(i=1;i<=a&&i<=b;i++)
+    {
+        if(a%i==0 && b%i==0)
         {
-            printf("Win
-");
+            g=i;
         }
-        else
-        {
-            printf("Lose
+    }
+    l=(a*b)/g;
+    c=(n/a)+(n/b)-(n*2)/l;
+    if(c>=k)
+    {
+        printf("Win
 ");
-        }
+    }
+    else
+    {
+        printf("Lose
+");
+    }
     }
 }
