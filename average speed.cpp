7506603356c78bc997ec25934fc49ff74b546eb6
sandhya <120093615+medapatisandhya@users.noreply.github.com>
2023-06-11 19:39:03 +0530

    Add files via upload

---
+++ average speed.cpp
@@ -0,0 +1,47 @@
+#include<stdio.h>
+int main()
+{
+    int n,arr[100];
+    int skip[100];
+    int sum=0;
+    int items=0;
+    int i,n;
+    double res;
+    scanf("%d",&n);
+    for(i=0;i<n;i++)
+    {
+        scanf("%d",&arr[i]);
+        skip[i]=0;
+    }
+    for(i=0;i<n;i++)
+    {
+        int count=0;
+        if(skip[i]) continue;
+        for(j=0;j<n;j++)
+        {
+            if(arr[i]==arr[j])
+            {
+                if(arr[i]==arr[j])
+                {
+                    ++count;
+                    skip[j]=l;
+                }
+            }
+            if(arr[i]==count)
+            {
+                sum+=arr[i];
+                ++items;
+            }
+        }
+        if(items==0)
+        {
+            printf("-1");
+        }
+        else
+        {
+            res=((double)sum)/items;
+            printf("%.2f",res);
+        }
+        return 0;
+    }
+}
